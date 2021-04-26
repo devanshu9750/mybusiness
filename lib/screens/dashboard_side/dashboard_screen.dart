b9622adb9fa6d98a1765368f74d8f18223f4818e
devanshu9750 <devg9750@gmail.com>
2021-04-26 17:02:58 +0530

    Added total outstanding and total revenue (for last 30 days) in dashboard screen

--- lib/screens/dashboard_side/dashboard_screen.dart
+++ lib/screens/dashboard_side/dashboard_screen.dart
@@ -1,9 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:mybusiness/models/client_model.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return VStack([
+      Card(
+        child: ListTile(
+          title: "Outstanding :".text.bold.make(),
+          subtitle: "Total".text.semiBold.make(),
+          trailing: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection('clients').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                int outstanding = 0;
+                snapshot.data?.docs.forEach((doc) {
+                  Client client = Client.fromJson(doc.data(), doc.id);
+                  int subtotal = 0;
+                  client.transactions.forEach((transaction) {
+                    if (transaction['credit'])
+                      subtotal += transaction['amount'] as int;
+                    else
+                      subtotal -= transaction['amount'] as int;
+                  });
+                  outstanding += subtotal;
+                });
+                return '₹ $outstanding'
+                    .text
+                    .color(Colors.red)
+                    .size(18)
+                    .bold
+                    .make();
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ).p(10),
+      Card(
+        child: ListTile(
+          title: "Total Revenue :".text.bold.make(),
+          subtitle: "For last 30 days".text.semiBold.make(),
+          trailing: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection('clients').snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                int revenue = 0;
+                snapshot.data?.docs.forEach((doc) {
+                  Client client = Client.fromJson(doc.data(), doc.id);
+                  client.transactions.forEach((transaction) {
+                    if ((transaction['time'] as Timestamp).compareTo(
+                            Timestamp.fromDate(DateTime(DateTime.now().year,
+                                DateTime.now().month, 1))) >
+                        0) if (transaction['credit'])
+                      revenue += transaction['amount'] as int;
+                  });
+                });
+                return '₹ +$revenue'
+                    .text
+                    .bold
+                    .color(Colors.green)
+                    .size(18)
+                    .make();
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
+      ).p(10)
+    ]).scrollVertical();
   }
 }
