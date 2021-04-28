71e55908140a3049ddef6e2d2a2a961a826de64c
devanshu9750 <devg9750@gmail.com>
2021-04-28 14:24:53 +0530

    Added feature to edit and delete personal expense

--- lib/screens/personal_expense/personal_expenses_screen.dart
+++ lib/screens/personal_expense/personal_expenses_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mybusiness/models/personal_expense_modal.dart';
@@ -42,7 +43,110 @@ class PersonalExpensesScreen extends StatelessWidget {
                   .color(Colors.red)
                   .size(18)
                   .make(),
-            ),
+            ).onInkLongPress(() {
+              showModal(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text("What would you like to do ?"),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        context.pop();
+                      },
+                      child: Text("Cancel"),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          String _amount =
+                                  personalexpenses[index].amount.toString(),
+                              _message = personalexpenses[index].message;
+                          showModal(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text("Edit"),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    context.pop();
+                                  },
+                                  child: Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () async {
+                                    await FirebaseFirestore.instance
+                                        .collection('personalexpense')
+                                        .doc(personalexpenses[index].id)
+                                        .update({
+                                      'amount': int.parse(_amount),
+                                      'message': _message
+                                    });
+                                    context.pop();
+                                    context.pop();
+                                  },
+                                  child: Text('Save'),
+                                )
+                              ],
+                              content: VStack([
+                                Text("Amount *"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                VxTextField(
+                                  hint: "Amount",
+                                  keyboardType: TextInputType.number,
+                                  onChanged: (value) => _amount = value.trim(),
+                                  borderType: VxTextFieldBorderType.roundLine,
+                                  borderRadius: 10,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text("Message (optional)"),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                VxTextField(
+                                  hint: 'Message',
+                                  onChanged: (value) => _message = value,
+                                  borderType: VxTextFieldBorderType.roundLine,
+                                  borderRadius: 10,
+                                )
+                              ]),
+                            ),
+                          );
+                        },
+                        child: Text("Edit")),
+                    TextButton(
+                        onPressed: () {
+                          showModal(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text("Are you sure ?"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      context.pop();
+                                    },
+                                    child: Text("No")),
+                                TextButton(
+                                    onPressed: () async {
+                                      await FirebaseFirestore.instance
+                                          .collection('personalexpense')
+                                          .doc(personalexpenses[index].id)
+                                          .delete();
+                                      context.pop();
+                                      context.pop();
+                                    },
+                                    child: Text("Yes")),
+                              ],
+                            ),
+                          );
+                        },
+                        child: Text("Delete")),
+                  ],
+                ),
+              );
+            }),
           );
         }
         return Center(child: CircularProgressIndicator());
